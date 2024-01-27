e82ac7a16aa2228a213a0812e828abc2a1a16e86
TanviShingate6 <157889556+TanviShingate6@users.noreply.github.com>
2024-01-27 21:24:13 +0530

    commit1

--- sum.py
+++ sum.py
@@ -2,4 +2,4 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 total = num1 + num2
-print(f"The addition of {num1} and {num2} is: {total}")
+print(f"The sum of {num1} and {num2} is: {total}")
